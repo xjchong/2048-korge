35dac588fffab12b38551f13c72e456c842574ca
Xavier <xavier.chong.j@gmail.com>
2020-10-20 18:35:28 -0400

    Update colors for blocks

--- src/commonMain/kotlin/constants/GameColors.kt
+++ src/commonMain/kotlin/constants/GameColors.kt
@@ -1,6 +1,7 @@
 package constants
 
 import com.soywiz.korim.color.Colors
+import com.soywiz.korim.color.RGBA
 
 object GameColors {
 
@@ -10,15 +11,21 @@ object GameColors {
     val EMPTY_CELL_BACKGROUND = Colors["#cec0b2"]
     val TITLE_BACKGROUND = Colors["#edc403"]
 
-//    val CELL_2 = Colors[""]
-//    val CELL_4 = Colors[""]
-//    val CELL_8 = Colors[""]
-//    val CELL_16 = Colors[""]
-//    val CELL_32 = Colors[""]
-//    val CELL_64 = Colors[""]
-//    val CELL_128 = Colors[""]
-//    val CELL_256 = Colors[""]
-//    val CELL_512 = Colors[""]
-//    val CELL_1024 = Colors[""]
-//    val CELL_2048 = Colors[""]
+    val CELL_2 = RGBA(240, 228, 218)
+    val CELL_4 = RGBA(236, 224, 201)
+    val CELL_8 = RGBA(255, 178, 120)
+    val CELL_16 = RGBA(254, 150, 92)
+    val CELL_32 = RGBA(247, 123, 97)
+    val CELL_64 = RGBA(235, 88, 55)
+    val CELL_128 = RGBA(236, 220, 146)
+    val CELL_256 = RGBA(240, 212, 121)
+    val CELL_512 = RGBA(244, 206, 96)
+    val CELL_1024 = RGBA(248, 200, 71)
+    val CELL_2048 = RGBA(256, 194, 46)
+    val CELL_4096 = RGBA(256, 194, 46)
+    val CELL_8192 = RGBA(256, 194, 46)
+    val CELL_16384 = RGBA(256, 194, 46)
+    val CELL_32768 = RGBA(256, 194, 46)
+    val CELL_65536 = RGBA(256, 194, 46)
+    val CELL_131072 = RGBA(256, 194, 46)
 }
--- src/commonMain/kotlin/views/BlockView.kt
+++ src/commonMain/kotlin/views/BlockView.kt
@@ -4,6 +4,7 @@ import com.soywiz.korge.view.*
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.color.RGBA
 import constants.Dimensions
+import constants.GameColors
 import constants.GameConfig
 
 class BlockView(val number: Int) : Container() {
@@ -11,17 +12,38 @@ class BlockView(val number: Int) : Container() {
     companion object {
 
         private fun backgroundColorForNumber(number: Int): RGBA {
-            return Colors.RED
+            with (GameColors) {
+                return when (number) {
+                    2 -> CELL_2
+                    4 -> CELL_4
+                    8 -> CELL_8
+                    16 -> CELL_16
+                    32 -> CELL_32
+                    64 -> CELL_64
+                    128 -> CELL_128
+                    256 -> CELL_256
+                    512 -> CELL_512
+                    1024 -> CELL_1024
+                    2048 -> CELL_2048
+                    4096 -> CELL_4096
+                    8192 -> CELL_8192
+                    16384 -> CELL_16384
+                    32768 -> CELL_32768
+                    65536 -> CELL_65536
+                    131072 -> CELL_131072
+                    else -> Colors.BLACK
+                }
+            }
         }
 
         private fun textColorForNumber(number: Int): RGBA {
-            return Colors.WHITE
+            return if (number <= 4) Colors.BLACK else Colors.WHITE
         }
     }
 
     init {
         val title = number.toString()
-        val textSize = (Dimensions.CELL_WIDTH - (2 * Dimensions.CELL_PADDING)) / title.length
+        val textSize = (Dimensions.CELL_WIDTH - (2 * Dimensions.CELL_PADDING)) / (1 + (0.1 * title.length))
 
         val blockRect = roundRect(
                 Dimensions.CELL_WIDTH,
